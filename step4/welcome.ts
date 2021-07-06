b4510456fb4575ec6ea604ff484b13baa8f67fbc
Momo Kornher <mk@administrate.co>
2021-07-06 22:16:27 +0100

    style(step4): formatting

--- step4/welcome.ts
+++ step4/welcome.ts
@@ -1,12 +1,9 @@
-import {
-  red,
-  bold,
-  italic,
-  cyan
-} from "https://deno.land/std@v0.32.0/fmt/colors.ts";
+import { bold, cyan, italic, red } from "https://deno.land/std/fmt/colors.ts";
 
 console.log(
-  `${red(bold(Deno.args[0] || "Anonymous"))}, welcome to ${cyan(
-    italic("Deno")
-  )} 🦕🦕`
+  `${red(bold(Deno.args[0] || "Anonymous"))}, welcome to ${
+    cyan(
+      italic("Deno"),
+    )
+  } 🦕🦕`,
 );
